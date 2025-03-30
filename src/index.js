538effe8ebd09b5e299aa16d82a4804fa1c6fcc6
PamPT <p.pachecotorrico@gmail.com>
2025-03-30 19:06:15 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generateSong(event) {
   let apiKey = "ca301o346f2bf4fa483tcabaf1a295d2";
   let prompt = `User instructions: Generate a love song that includes ${instructionsInput.value}`;
   let context =
-    "You are a romantic song expert and love to write short love songs. Your mission is to generate a short love song, separated in verses, in basic HTML and separate each line with a  <br />.  Do not include a title to the song. Do not write html at the beggining.";
+    "You are a romantic song expert and love to write short love songs. Your mission is to generate a short love song, always separate it in verses, in basic HTML and separate each line with a  <br />.  Do not include a title to the song. Do not write html at the beggining.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let songElement = document.querySelector("#song");
+  songElement.classList.remove("hidden");
+  songElement.innerHTML = `<span class="blink">⌛Generating a song that includes ${instructionsInput.value} for you...</span>`;
 
   console.log("Generating Song");
   console.log(`Prompt: ${prompt}`);
